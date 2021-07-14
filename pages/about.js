79f24fdb83655b3affa8b574cd7ac7b85fc0da00
ankaahmadk <anka.ahmad.k@gmail.com>
2021-07-14 13:53:36 +0300

    about us
    I added the about as class component to showcase my knowledge in both the class component and functional components

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class About extends React.Component {
+
+    render() {
+        return <div>
+            <p>The WeatherAPP will allow you to search for you city and find its current weather condition</p>
+            <p>you can click on the search result and get more information about the forcast for the next 7 days </p>
+        </div>
+    }
+}
+
+export default About
